6bc022c43e92e460b81e64cba87076a252a4e0cc
Franzi2110 <90309752+Franzi2110@users.noreply.github.com>
2021-10-08 17:30:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function showDetails(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   let showHint = response.data.weather[0].icon;
-  if (showHint === "09d" || "10d") {
+  if (showHint === "09d" || showHint === "10d") {
     document.querySelector("#hint").innerHTML = "Don't forget your umbrella!";
   } else {
     if (showHint === "01d") {
