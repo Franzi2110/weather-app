56ed4cbe56aa474065f9ef713513614b0170648c
Franzi2110 <90309752+Franzi2110@users.noreply.github.com>
2021-10-13 12:57:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,9 @@ function showForecast(response) {
       `
             <div class="col-2">
               <div class="weather-forecast-date">${forecastDay.dt}</div>
-              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"/>
+              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+              alt=""
+              width="42"/>
               <div class="weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">${forecastDay.temp.max}°</span>
                 <span class="weather-forecast-temperature-min">${forecastDay.temp.max}°</span>
