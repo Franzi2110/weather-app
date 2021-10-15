72b7ed416148896d252fede783c356566f729d03
Franzi2110 <90309752+Franzi2110@users.noreply.github.com>
2021-10-15 09:47:57 +0200

    updated index.js to align weather forecast icon

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ function showForecast(response) {
               }@2x.png"
               alt=""
               width="42"
+              text-align="center"
               class="forecast-icon"/>
               <div class="weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">${Math.round(
